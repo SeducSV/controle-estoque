1872a9d225035bc231a2b163fa6b45ad9fb69970
Pedroca <pmsvtec@gmail.com>
2024-05-22 09:39:33 -0300

    metodos de usuarios feitos

--- models/Usuario.php
+++ models/Usuario.php
@@ -37,11 +37,105 @@ class Usuario extends DbConnect
             if($stmt->rowCount() > 0) {
                 return false;
             } else {
-                $hashSenha = password_hash($senhaUsuario, 8);
+                $hashSenha = password_hash($senhaUsuario, PASSWORD_DEFAULT);
                 $stmt2 = $pdo->prepare("INSERT INTO usuarios VALUES (null, ?, ?, ?)");
                 $stmt2->execute([$nomeUsuario, $emailUsuario, $hashSenha, $idTipoUsuario]);
                 return true;
             }
+        } catch (PDOException $e) {
+            echo "Erro com o banco de dados: " .$e;
+        }
+    }
+
+    public static function deletarUsuario($idUsuario) {
+        $pdo = DbConnect::realizarConexao();
+
+        try {
+            $stmt = $pdo->prepare("DELETE FROM usuarios WHERE idUsuario = ?");
+            $stmt->execute([$idUsuario]);
+
+            return true;
+        } catch (PDOException $e) {
+            echo "Erro com o banco de dados: " .$e;
+        }
+    }
+
+    public static function atualizarUsuario($idUsuario, $nomeUsuario, $emailUsuario, $idTipoUsuario) {
+        $pdo = DbConnect::realizarConexao();
+
+        try {
+            $stmt = $pdo->prepare("UPDATE usuarios SET nomeUsuario = ?, emailUsuario = ?, tipoUsuario = ? WHERE idUsuario = ?");
+            $stmt->execute([$nomeUsuario, $emailUsuario, $idTipoUsuario, $idUsuario]);
+
+            return true;
+        } catch (PDOException $e) {
+            echo "Erro com o banco de dados: " .$e;
+        }
+    }
+
+    public static function atualizarSenha($idUsuario, $senhaAntiga, $senhaNova) {
+        $pdo = DbConnect::realizarConexao();
+
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE idUsuario = ?");
+            $stmt->execute([$idUsuario]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if(password_verify($senhaAntiga, $result['senhaUsuario'])) {
+                $hashSenha = password_hash($senhaNova, PASSWORD_DEFAULT);
+
+                $stm = $pdo->prepare("UPDATE usuarios SET senhaUsuario = ? WHERE idUsuario = ?");
+                $stm->execute([$hashSenha, $idUsuario]);
+
+                return true;
+
+            } else {
+                return false;
+            }
+
+        } catch (PDOException $e) {
+            echo "Erro com o banco de dados: " .$e;
+        }
+    }
+
+    public static function listarUsuarios() {
+        $pdo = DbConnect::realizarConexao();
+
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM usuarios");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $result;
+        } catch (PDOException $e) {
+            echo "Erro com o banco de dados: " .$e;
+        }
+    }
+
+    public static function selecionarUsuario($idUsuario) {
+        $pdo = DbConnect::realizarConexao();
+
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE idUsuario = ?");
+            $stmt->execute([$idUsuario]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            return $result;
+
+        } catch (PDOException $e) {
+            echo "Erro com o banco de dados: " .$e;
+        }
+    }
+
+    public static function listarTiposUsuarios() {
+        $pdo = DbConnect::realizarConexao();
+
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM tipos_usuarios");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $result;
         } catch (PDOException $e) {
             echo "Erro com o banco de dados: " .$e;
         }
