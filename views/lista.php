bc453497873e595e2365a14b87314e13ebf81bff
SEDUC-EstagSV <pmsvtec@gmail.com>
2024-05-23 08:12:01 -0300

    header alterado

--- views/lista.php
+++ views/lista.php
@@ -17,26 +17,8 @@ $lista = Equipamento::listarEquipamentos();
 </head>
 
 <body onresize="mudouTamanho()">
-    <header>
-        <nav class="nav">
-            <a href='home'>
-                <picture>
-                    <source media="(max-width:962px)" srcset="../public/imgs/logo.jpg">
-                    <a href="http://localhost/controle-estoque/views/adicionar.php">
-                    <img src="../public/imgs/logo.png" alt="Logo da Prefeitura de São Vicente">
-                    </a>
-                </picture>
-            </a>
-            <ul id="itens">
-            <li><a href="lista.php">Lista</a></li>
-                <li><a href="entrada.php">Entrada</a></li>
-                <li><a href="saida.php">Saida</a></li>
-                <li><a href="adicionar.php">Adicionar equipamentos</a></li>
-                <li><a href="cadastrar.php">Cadastrar usuário</a></li>
-            </ul>
-        </nav>
-        <i class="fa-solid fa-bars" id="burguer" onclick="clickMenu()"></i>
-    </header>
+<?php include 'header.php'; ?>
+
     <main>
         <h1>
             Lista de Equipamentos
